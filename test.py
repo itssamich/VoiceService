8a8f85fc82425a9e1104baadc4245610f7a784c3
itssamich <alexanderklein@mail.weber.edu>
2021-06-06 17:41:56 -0600

    Added basic math operators from voiceString

--- test.py
+++ test.py
@@ -15,15 +15,23 @@ engine = pyttsx3.init() #Text-to-Speech library
 with sr.Microphone() as source:
     print("start")
     audio = r.listen(source)
-voiceString = r.recognize_google(audio)
+voiceString = r.recognize_google(audio).lower()
 
 #Allows user to cancel the processing of their voice clip if contains "cancel" in the string
 if "cancel" not in voiceString:
     try:
         #For now this just prints out what google voice recognition services thinks was said and then has pyttsx3 read aloud what was saved by google
         print("Google thinks I said \"" + voiceString + "\"")
-        engine.say(voiceString)
+        #engine.say(voiceString)
+
+        #This could be abusable and as such is not a safe method if input is trusted. I think the operator check would suffice to ensure that a valid input is being processed. Can evaluate later
+        if "+" in voiceString or "-" in voiceString or "*" in voiceString or "/" in voiceString:
+            stringtest = voiceString.replace("what's ", "")
+            engine.say(eval(stringtest))
+        
+
         engine.runAndWait()
+
     except sr.UnknownValueError:
         print("Google could not understand audio")
     except sr.RequestError as e:
